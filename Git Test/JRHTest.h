7fcb93f08a48566a8aa5d9f6b3da14e915e53f4e
Jennifer Hoover <jenniferhoover@Jennifers-MacBook-Pro.local>
2014-08-01 09:43:50 -0700

    added a test class

---
+++ Git Test/JRHTest.h
@@ -0,0 +1,13 @@
+//
+//  JRHTest.h
+//  Git Test
+//
+//  Created by Jennifer Hoover on 8/1/14.
+//  Copyright (c) 2014 Jennifer Hoover. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JRHTest : NSObject
+
+@end
---
+++ Git Test/JRHTest.m
@@ -0,0 +1,13 @@
+//
+//  JRHTest.m
+//  Git Test
+//
+//  Created by Jennifer Hoover on 8/1/14.
+//  Copyright (c) 2014 Jennifer Hoover. All rights reserved.
+//
+
+#import "JRHTest.h"
+
+@implementation JRHTest
+
+@end
